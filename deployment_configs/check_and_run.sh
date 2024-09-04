f88a91f72786aceb8e916124d1a58f285d1dc466
jahoroshi4y <jahoroshi4y@gmail.com>
2024-09-04 11:44:31 +0300

    Application deployment. Check and run.

---
+++ deployment_configs/check_and_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PORT=8000
+
+if lsof -i :$PORT > /dev/null; then
+    echo "Port $PORT is already in use. Killing all processes using the port."
+    lsof -t -i :$PORT | xargs -r kill -9
+    sleep 2
+fi
+
+exec /home/ubuntu/.cache/pypoetry/virtualenvs/ankichat-IcZeuUhH-py3.12/bin/gunicorn --workers 3 --bind 0.0.0.0:8000 -c /home/ubuntu/ankichat/deployment_configs/gunicorn.conf -k uvicorn.workers.UvicornWorker asgi:application
